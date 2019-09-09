560f69862dd74b6b9b663f4e67944cc3912c23d5
Thiago Lemos <thiagolemos95@outlook.com>
2019-09-09 17:26:12 -0300

    teste

--- index.js
+++ index.js
@@ -1,18 +1,6 @@
 var slideIndex = 0;
 const slides = [
   {
-    name: 'Burndown',
-    page:
-      'https://docs.google.com/spreadsheets/d/e/2PACX-1vQvcXuN_MWoenkykYHf4FEa832STbgvAqV_Tc06Rn6ToSAAl6TXHSvOR4Mw3X7Md-1k-KJ05yFBJaTF/pubchart?oid=1745078327&format=image',
-    seconds: 60
-  },
-  {
-    name: 'Atividades',
-    page:
-      'https://docs.google.com/spreadsheets/d/1H7qHm-CM-yvAcnt99wA_eTaV3W2ss6sXGhwsOa5xlXI/edit#gid=379786966',
-    seconds: 60
-  },
-  {
     name: 'Jetsons Farm',
     page:
       'https://docs.google.com/spreadsheets/u/1/d/e/2PACX-1vSfkUzbPxmPQA49Az1tXINQ6MpfOHVq7rFthvikqkNIVADKThgOVCSrue_WEbOuBZ8i1M5L5FP9FjZV/pubhtml?gid=681837777&single=true',
