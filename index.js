2592f21d39ed731f309065821c81a26652eea169
Thiago Lemos <thiagolemos95@outlook.com>
2019-11-04 17:50:36 -0300

    Alteração de tempo de mudança das imagens

--- index.js
+++ index.js
@@ -4,19 +4,19 @@ const slides = [
     name: 'Jetsons Farm',
     page:
       'https://docs.google.com/spreadsheets/u/1/d/e/2PACX-1vSfkUzbPxmPQA49Az1tXINQ6MpfOHVq7rFthvikqkNIVADKThgOVCSrue_WEbOuBZ8i1M5L5FP9FjZV/pubhtml?gid=681837777&single=true',
-    seconds: 5
+    seconds: 20
   },
    {
     name: 'Racha Cuca',
     page:
       'https://docs.google.com/spreadsheets/d/e/2PACX-1vRNCzg2yt698kYJd0reE6PpXT7lKgNB2rlO4-rAgHCd59H3HOjm6DJOy-46A6P7mKWENq_8-gUPbH2j/pubhtml?gid=1082556539&single=true',
-    seconds: 5
+    seconds: 20
   },
   {
     name: 'Racha Cuca',
     page:
       'https://docs.google.com/spreadsheets/d/e/2PACX-1vQJBkN1CbqskTHHnDlV-LQCzHBCtC9DniWaIFOdrmzTDiOHdmUJ7opwtJyGXviMVFE9Za4yX75to-At/pubhtml?gid=1082556539&single=true',
-    seconds: 5
+    seconds: 20
   }
 ];
 
